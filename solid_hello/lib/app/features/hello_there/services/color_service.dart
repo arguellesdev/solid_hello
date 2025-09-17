683141cbbfba6515416e02b6a8fabb38bb511f69
Anya <arguellesdev@gmail.com>
2025-09-17 08:32:23 -0700

    adding color service logic separated

---
+++ solid_hello/lib/app/features/hello_there/services/color_service.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ColorService {
+  final Random _rng = Random();
+
+  Color generateRandomColor() {
+    return Color.fromARGB(
+      255,
+      _rng.nextInt(256),
+      _rng.nextInt(256),
+      _rng.nextInt(256),
+    );
+  }
+
+  Color generate24BitColor() {
+    final rgb = _rng.nextInt(1 << 24);
+    return Color(0xFF000000 | rgb);
+  }
+
+  Color readableOn(Color bg) {
+    return bg.computeLuminance() > 0.6 ? Colors.black : Colors.white;
+  }
+
+  String hex(Color color) {
+    return '#${color.toARGB32().toRadixString(16).padLeft(8, '0').substring(2).toUpperCase()}';
+  }
+}
